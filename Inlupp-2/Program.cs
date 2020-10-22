eb1def12ca7441f5b73e9d2cdc77e9d22b9f857f
rebeccadannerhall <rebeccadannerhall@gmail.com>
2020-10-22 15:18:31 +0200

    Initial commit

---
+++ Inlupp-2/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Inlupp_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            //Kassa go
+        }
+
+        
+    }
+
+    class Kassa
+    {
+        public void Run()
+        {
+            var quit = false;
+            while (!quit)
+            {
+                Console.WriteLine("1. Ny kund");
+                Console.WriteLine("0. Avsluta");
+                int selection = int.Parse(Console.ReadLine());
+                if (selection == 1)
+                {
+                    //Kvittoprinter go
+                }
+                else
+                {
+                    quit = true;
+                }
+            }
+        }
+    }
+
+
+}
